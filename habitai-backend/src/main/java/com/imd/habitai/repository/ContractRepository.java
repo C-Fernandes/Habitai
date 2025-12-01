5bf4cd4c1ac2092086d4b36121468163280629e5
Henrique Barbosa <henrique1508cops@gmail.com>
2025-12-01 10:34:56 -0300

    [feat] Query em SQL puro para filtragem pro inquilino e imóvel nos Contracts

--- habitai-backend/src/main/java/com/imd/habitai/repository/ContractRepository.java
+++ habitai-backend/src/main/java/com/imd/habitai/repository/ContractRepository.java
@@ -3,6 +3,8 @@ package com.imd.habitai.repository;
 import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import com.imd.habitai.model.Contract;
 import com.imd.habitai.model.Property;
@@ -12,5 +14,14 @@ public interface ContractRepository extends JpaRepository<Contract, Long>{
     public List<Contract> findAllByOwner(User owner);
     public List<Contract> findAllByTenant(User tenant);
     public List<Contract> findAllByProperty(Property property);
-    public boolean existsByTenantIdAndPropertyId(Long userId, Long propertyId);
+
+    @Query(value = """
+    SELECT EXISTS (
+        SELECT 1 
+        FROM contracts 
+        WHERE tenant_id = :userId 
+          AND property_id = :propertyId
+    )
+    """, nativeQuery = true)
+    boolean existsByTenantIdAndPropertyId(@Param("userId") Long userId, @Param("propertyId") Long propertyId);
 }
